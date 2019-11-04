042465b85ed7e1cb6e2484cf5851f97c621b2e27
shengxinjie <928239273@qq.com>
2019-11-04 23:13:00 +0800

    add

--- leet_code.py
+++ leet_code.py
@@ -1040,15 +1040,43 @@
 
 # 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
 
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        if max(nums) < 0:
-            return max(nums)
-        local_max = 0
-        global_max = 0
-        for num in nums:
-            local_max = max(0, local_max+num)
-            global_max = max(local_max, global_max)
-        return global_max
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         if max(nums) < 0:
+#             return max(nums)
+#         local_max = 0
+#         global_max = 0
+#         for num in nums:
+#             local_max = max(0, local_max+num)
+#             global_max = max(local_max, global_max)
+#         return global_max
 
-         
+
+# 62. 不同路径
+# 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+# 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+
+# 问总共有多少条不同的路径？
+# 说明：m 和 n 的值均不超过 100。
+
+# 示例 1:
+
+# 输入: m = 3, n = 2
+# 输出: 3
+# 解释:
+# 从左上角开始，总共有 3 条路径可以到达右下角。
+# 1. 向右 -> 向右 -> 向下
+# 2. 向右 -> 向下 -> 向右
+# 3. 向下 -> 向右 -> 向右
+
+class Solution:
+    def uniquePaths(self, m, n):
+        dp = [[1]*n] + [[1]+[0] * (n-1) for _ in range(m-1)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+
+ret = Solution().uniquePaths(3, 2)
+print(ret)

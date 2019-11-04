426c37fe705b758f6dc4320b10614ea770fbe371
shengxinjie <928239273@qq.com>
2019-11-04 22:40:58 +0800

    add

--- leet_code.py
+++ leet_code.py
@@ -1026,3 +1026,29 @@
 # s = "babad"
 # ret = Solution().longestPalindrome(s)
 # print(ret)
+
+
+# 53. 最大子序和
+# 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+# 示例:
+
+# 输入: [-2,1,-3,4,-1,2,1,-5,4],
+# 输出: 6
+# 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+# 进阶:
+
+# 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if max(nums) < 0:
+            return max(nums)
+        local_max = 0
+        global_max = 0
+        for num in nums:
+            local_max = max(0, local_max+num)
+            global_max = max(local_max, global_max)
+        return global_max
+
+         

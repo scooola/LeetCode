3ef19d730a63f569649c7932e763dde2a10bb145
shengxinjie <928239273@qq.com>
2019-11-04 22:03:26 +0800

    add

--- leet_code.py
+++ leet_code.py
@@ -966,26 +966,63 @@
 # 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
 #      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        max_len = 0
-        cur_len = 0
-        window = set()
-        for i in range(len(s)):
-            cur_len += 1
-            while s[i] in window:
-                cur_len -= 1
-                window.remove(s[left])
-                left += 1
-            if cur_len > max_len:
-                max_len = cur_len
-            window.add(s[i])
-        return max_len
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         left = 0
+#         max_len = 0
+#         cur_len = 0
+#         window = set()
+#         for i in range(len(s)):
+#             cur_len += 1
+#             while s[i] in window:
+#                 cur_len -= 1
+#                 window.remove(s[left])
+#                 left += 1
+#             if cur_len > max_len:
+#                 max_len = cur_len
+#             window.add(s[i])
+#         return max_len
 
 
-s = "bbbbb"
-ret = Solution().lengthOfLongestSubstring(s)
-print(s)
-            
+# s = "bbbbb"
+# ret = Solution().lengthOfLongestSubstring(s)
+# print(s)
 
+
+# 5. 最长回文子串-动态规划
+# 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+# 示例 1：
+
+# 输入: "babad"
+# 输出: "bab"
+# 注意: "aba" 也是一个有效答案。
+# 示例 2：
+
+# 输入: "cbbd"
+# 输出: "bb"
+
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         result = ''
+#         for i in range(len(s)):
+#             str1 = self.getLongestPalindrome(s, i, i)
+#             if len(str1) >len(result):
+#                 result = str1
+#             str2 = self.getLongestPalindrome(s, i, i+1)
+#             if len(str2) >len(result):
+#                 result = str2
+        
+#         return result
+
+#     def getLongestPalindrome(self, s, left, right):
+#         while left >= 0 and right < len(s) and s[left] == s[right]:
+#             left -= 1
+#             right += 1
+        
+#         return s[left+1:right]
+
+
+# s = "babad"
+# ret = Solution().longestPalindrome(s)
+# print(ret)
